ae1b99b8615b00eaa761611eb531c6c1468b16ff
amir sohail <9902699141a@gmail.com>
2022-04-29 15:50:32 +0530

    Bubble detection using Finger sphere collider

---
+++ Assets/Avatar/Scripts/BubbleCollide.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleCollide : MonoBehaviour
+{
+    public Transform PlayerColliderGet;
+   private void Start() {
+       
+        Physics.IgnoreCollision(PlayerColliderGet.GetComponent<Collider>(), GetComponent<Collider>(),true);
+   }
+    
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.tag == "Bubble")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Finger Sphere collider collided with  Bubble");
+        }
+
+    }
+}
--- Assets/Avatar/Scripts/PlayerScript.cs
+++ Assets/Avatar/Scripts/PlayerScript.cs
@@ -7,7 +7,7 @@ public class PlayerScript : MonoBehaviour
 {
      private Transform finger;
      public Animator animator;
-     public GameObject mParentComp;
+    //  public GameObject mParentComp;
 
     //  public GameObject RuntimeTarget;
     private bool push;
@@ -20,7 +20,9 @@ public class PlayerScript : MonoBehaviour
 
          
           // Find finger and raycast
-         finger = GameObject.Find("RightHandIndex4").transform;
+        //  finger = GameObject.Find("RightHandIndex4").transform;
+        finger = GameObject.Find("RaycastPoint").transform;
+         
          
          if (finger)
          {
@@ -39,23 +41,40 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Ray casting code
-            RaycastHit hit;
-            float maxDistance = 1f;
-            var direction = finger.forward;
-            var origin = finger.position;
-            var Ray = Physics.Raycast(origin,direction, out  hit, maxDistance);
+        // // Ray casting code
 
-            if(Ray && hit.collider.tag != "Player"  )
-            {
-                // Vector3 forward = origin;
-                // Debug.DrawRay(transform.position, forward, Color.green);              
+        //     float maxRayDistance = 1f;
+        //     var direction = finger.forward;
+        //     var origin = finger.position;
+        //     var Ray = Physics.Raycast(origin,direction, out RaycastHit hit, maxRayDistance);
 
-                if (hit.collider.tag == "Finish" && hit.collider.tag != "Player" )
-                    {
-                        // gameObject.SetActive(false);
-                    }   
-            }
+            
+
+            
+
+        //     if(Ray && hit.collider.tag != "Player" )
+        //     {
+        //         Debug.Log("hit somethig");
+        //         // Vector3 forward = origin;
+        //         // Debug.DrawRay(transform.position, forward, Color.green);
+
+        //         Debug.DrawLine(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+                
+                              
+
+        //         if (hit.collider.tag == "Bubble"  )
+        //             {
+        //                 // gameObject.SetActive(false);
+        //                 Debug.Log("hit bubble");
+                        
+        //             }   
+        //     }
+        //     else
+        //     {
+        //         // Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+        //         Debug.Log("Did not Hit");
+        //         Debug.DrawLine(transform.position, transform.TransformDirection(Vector3.forward) * 0.1f, Color.green);
+        //     }
         
 
         //Click the mouse or tap the screen to change the animation
